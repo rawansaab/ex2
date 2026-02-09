5c6f16af14ac0be48045c0124130b45b9dda22ed
Rawan Saab <rawansaab777@gmail.com>
2026-02-09 22:35:02 +0200

    feat: add Express server with /profile route

---
+++ profile.js
@@ -0,0 +1,7 @@
+/**
+ * Names: Rawan Saab: 213693625, Lareen Kadour: , Geroge Hanna: .
+ * Github URL: https://github.com/rawansaab/ex2
+ */
+
+const express = require("express");
+const app = express();
