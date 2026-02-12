0d3d01552119339614826a093bd582e67d6a5614
Rawan Saab <rawansaab777@gmail.com>
2026-02-12 20:09:01 +0200

    feat: open sqlite database connection

--- profile.js
+++ profile.js
@@ -6,9 +6,11 @@
 const express = require("express");
 const app = express();
 
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./private/profiles.db");
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-
 
 app.get("/profile", function (req, res) {
   let id = req.query.id;
