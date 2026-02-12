080c7e471c15a9843d974fc70ca53f35625d5ee7
Rawan Saab <rawansaab777@gmail.com>
2026-02-12 21:07:32 +0200

    feat: connect sqlite database for profiles

--- profile.js
+++ profile.js
@@ -5,9 +5,12 @@
 
 
 const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
 const PORT = 3000;
+
+const db = new sqlite3.Database("./private/profiles.db");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
