8f7496601a347ac801ba53faa54eb00142385bf9
Rawan Saab <rawansaab777@gmail.com>
2026-02-12 20:38:49 +0200

    feat: connect sqlite database for profiles

--- profile.js
+++ profile.js
@@ -1,27 +1,19 @@
 /**
- * Names: Rawan Saab: 213693625, Lareen Kadour: , Geroge Hanna: .
+ * Names: Rawan Saab: 213693625, Lareen Kadour: 213992431, George Hanna: 324090968
  * Github URL: https://github.com/rawansaab/ex2
  */
 
 const express = require("express");
-const app = express();
-
 const sqlite3 = require("sqlite3").verbose();
+
+const app = express();
+const PORT = 3000;
+
 const db = new sqlite3.Database("./private/profiles.db");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get("/profile", function (req, res) {
-  let id = req.query.id;
-
-  db.get("SELECT * FROM animals WHERE id = ?", [id], function (err, animal) {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    res.render("profile", { id: id, animal: animal });
-  });
+app.listen(PORT, function () {
+  console.log(`App listening on port ${PORT}`);
 });
-
-app.listen(3000);
