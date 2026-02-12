9fc01efc14d352e7df35d23001ea26fd71872cfb
Rawan Saab <rawansaab777@gmail.com>
2026-02-12 20:57:31 +0200

    feat: setup express server and ejs profile route

--- profile.js
+++ profile.js
@@ -3,16 +3,19 @@
  * Github URL: https://github.com/rawansaab/ex2
  */
 
+
 const express = require("express");
-const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
 const PORT = 3000;
 
-const db = new sqlite3.Database("./private/profiles.db");
-
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+app.get("/profile", function (req, res) {
+  const id = req.query.id;
+  res.render("profile", { id: id });
+});
 
 app.listen(PORT, function () {
   console.log(`App listening on port ${PORT}`);
