c6907e572e3e29d0bc12d04620955cf32cefe55a
Rawan Saab <rawansaab777@gmail.com>
2026-02-25 23:46:41 +0200

    feat: add express + ejs basic rendering with query id

--- profile.js
+++ profile.js
@@ -2,31 +2,22 @@
  * Names: Rawan Saab: 213693625, Lareen Kadour: 213992431, George Hanna: 324090968
  * Github URL: https://github.com/rawansaab/ex2
  */
- 
+
 const express = require("express");
-const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
-const db = new sqlite3.Database("./private/profiles.db");
-
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static("public"));
 
 app.get("/profile", function (req, res) {
   const id = req.query.id;
-
-  db.get("SELECT * FROM animals WHERE id = ?", [id], function (err, animal) {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Database error");
-      return;
-    }
-    res.render("profile", { id: id, animal: animal });
-  });
+  res.render("profile", { id: id });
 });
 
-app.listen(PORT, function () {
-  console.log(`App listening on port ${PORT}`);
+app.listen(port, function () {
+  console.log("Open: http://localhost:3000/profile?id=mickey");
 });
