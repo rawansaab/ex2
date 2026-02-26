5331f2147098548a04af88f8b41006be1b356aa1
Rawan Saab <rawansaab777@gmail.com>
2026-02-27 00:38:14 +0200

    Set up view engine and static files

--- profile.js
+++ profile.js
@@ -7,6 +7,10 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.listen(PORT, function () {
   console.log("Server is running on port 3000");
 });
