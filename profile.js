f0ef081eb3e34720745a5b35692214fbbe3f7170
Rawan Saab <rawansaab777@gmail.com>
2026-02-27 01:21:02 +0200

    Query animal data from database

--- profile.js
+++ profile.js
@@ -5,7 +5,7 @@
 
 const express = require("express");
 const path = require("path");
-const sqlite3 = require("sqlite3").verbose(); 
+const sqlite3 = require("sqlite3").verbose();
 const app = express();
 const PORT = 3000;
 
@@ -13,11 +13,19 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-const db = new sqlite3.Database("private/profiles.db");
+const dbPath = path.join(__dirname, "private", "profiles.db");
+const db = new sqlite3.Database(dbPath);
 
 app.get("/profile", function (req, res) {
   const id = req.params.id; 
-  res.render("profile", { id: id });
+
+  // animals שולפים את הנתונים מטבלת 
+  db.get("SELECT animal_name, description FROM animals WHERE animal_name = ?", [id], function (err, animalRow) {
+    res.render("profile", { 
+      id: id,
+      animal: animalRow
+    });
+  });
 });
 
 app.listen(PORT, function () {
