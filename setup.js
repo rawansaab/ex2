14a26c309fba1d6a5e0a19f6b7340b5209504546
Rawan Saab <rawansaab777@gmail.com>
2026-03-07 19:51:53 +0200

    initialize database setup script with connection settings

--- setup.js
+++ setup.js
@@ -1,3 +1,7 @@
 // Database setup script for myprofile
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
+
+// Define database path
+const dbPath = path.join(__dirname, "private", "profiles.db");
+const db = new sqlite3.Database(dbPath);
