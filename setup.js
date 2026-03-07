a31369052d463cce3c0ebb381bed2f9fb447ee66
Rawan Saab <rawansaab777@gmail.com>
2026-03-07 20:18:04 +0200

    complte the set of 4 personal traits

--- setup.js
+++ setup.js
@@ -12,4 +12,6 @@ db.run(`INSERT INTO animals (animal_name, description) VALUES ('myprofile', 'We 
 // Step 2: Adding 4 personal traits for the team
 db.run(`INSERT INTO animal_traits (animal_name, trait_name, trait_value) VALUES 
 ('myprofile', 'Names', 'Rawan, Lareen & George'),
-('myprofile', 'Academic Year', '2026')`);
+('myprofile', 'Academic Year', '2026'),
+('myprofile', 'Specialization', 'Information Systems'),
+('myprofile', 'Tech Stack', 'Node.js, Express, SQLite')`);
