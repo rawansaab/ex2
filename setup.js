3251f0bb37e0d06cdab36171fe94522d091047da
Rawan Saab <rawansaab777@gmail.com>
2026-03-07 20:33:02 +0200

    add initial profile reviews

--- setup.js
+++ setup.js
@@ -15,3 +15,8 @@ db.run(`INSERT INTO animal_traits (animal_name, trait_name, trait_value) VALUES
 ('myprofile', 'Academic Year', '2026'),
 ('myprofile', 'Specialization', 'Information Systems'),
 ('myprofile', 'Tech Stack', 'Node.js, Express, SQLite')`);
+
+// Step 3: Adding  personal reviews
+db.run(`INSERT INTO reviews (animal_name, review_number, review_text, reviewer) VALUES 
+('myprofile', 1, 'The team demonstrated great collaboration skills.', 'Senior Developer'),
+('myprofile', 2, 'The dynamic features are implemented perfectly.', 'Project Lead')`);
